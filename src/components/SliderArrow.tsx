232019559ec64da25885d279237dae1b03773fdc
y2code dev <y2codedev@gmail.com>
2025-05-14 18:54:26 +0530

    fix bugs

--- src/components/SliderArrow.tsx
+++ src/components/SliderArrow.tsx
@@ -1,27 +1,25 @@
-import { FaArrowLeftLong,FaArrowRight } from "react-icons/fa6";
-
-import React from 'react'
+import { FaArrowLeftLong, FaArrowRight } from "react-icons/fa6";
+import React from 'react';
 
 interface SliderArrowProps {
-  onClick?: () => void
-  direction: 'left' | 'right'
+  onClick?: () => void;
+  direction: 'left' | 'right';
 }
 
 const SliderArrow: React.FC<SliderArrowProps> = ({ onClick, direction }) => {
-  const isLeft = direction === 'left'
+  const isLeft = direction === 'left';
 
   return (
     <button
       onClick={onClick}
-      className={`absolute z-10 p-1 rounded-ful  sm:hidden justify-center items-center   text-white  transition-all ${
-        isLeft ? 'left-1' : 'right-1'
-      } top-1/2 -translate-y-1/2 md:top-1/2 md:-translate-y-1/2 md:left-6 md:right-6
-      bottom-4 md:bottom-auto`}
+      className={`absolute z-10 p-2 flex justify-center items-center  cursor-pointer rounded-full sm:shadow-md
+        top-1/2 -translate-y-1/2 
+        ${isLeft ? 'left-2 md:left-6' : 'right-2 md:right-6'}`}
       aria-label={isLeft ? 'Previous Slide' : 'Next Slide'}
     >
-      {isLeft ? <FaArrowLeftLong size={20} color="black"  /> : <FaArrowRight size={20} color="black"  />}
+      {isLeft ? <FaArrowLeftLong size={15} color="black" /> : <FaArrowRight size={15} color="black" />}
     </button>
-  )
-}
+  );
+};
 
-export default SliderArrow
+export default SliderArrow;
