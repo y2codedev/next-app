63adf77c55bc790879f2ef7a08ac104f17480906
y2code dev <y2codedev@gmail.com>
2025-05-23 18:04:27 +0530

    fix bugs

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     domains: ["shopv.swissblu.com", "cdn.keepconverting.ai", "cdn.dummyjson.com"],
   },
   reactStrictMode: false,
+  
 };
 
 export default nextConfig;
