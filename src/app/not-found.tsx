e11e4558e93d3fef753fbe0f8e920d92f067f31f
y2code dev <y2codedev@gmail.com>
2025-05-29 14:02:05 +0530

    fix bugs

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-3xl font-bold">Page Not Found</h1>
-      <p className="text-gray-500 mt-2">The page you're looking for doesn't exist.</p>
+      <p className="text-gray-500 mt-2">The page you are looking for does not exist</p>
     </div>
   );
 }
