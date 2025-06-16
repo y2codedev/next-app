19b1cf6bfb03e414da0c92a0f2b12e11b725b6c8
deepak <y2codedev@gmail.com>
2025-06-16 17:40:33 +0530

    fix bugs of fliter

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -79,7 +79,7 @@ const Sidebar = () => {
     if (selectedCategory) {
       localStorage.setItem("selectedCategory", selectedCategory);
     }
-  }, [selectedCategory,searchParams]);
+  }, [selectedCategory]);
 
   const renderSidebar = () => (
     <div className="w-full min-h-screen rounded-md  p-4 bg-white shadow-sm animate-fade-in space-y-6">
